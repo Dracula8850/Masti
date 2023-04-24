616998af4c33edc429ae165d0bb6acbf8ac97955
Dracula8850 <104322182+Dracula8850@users.noreply.github.com>
2023-04-24 10:12:34 +0530

    Update hey.java

--- hey.java
+++ hey.java
@@ -3,7 +3,7 @@ public class hey
 	public static void main(String[] args) {
           int x =5; //first arguments.
           int y = 10; //second arguments.
-          int sum = x + y;
-          System.out.println("The sum of x and y is: " +sum);
+          int multiply = x * y;
+          System.out.println("The sum of x and y is: " +multiply);
 	}
 }
